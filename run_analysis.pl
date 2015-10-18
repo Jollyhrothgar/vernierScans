a4b15f842268d2eed477b2fe4bfc43c0c597cc0d
Jollyhrothgar <michael.beaumier@gmail.com>
2015-10-17 18:17:44 -0700

    added perl script to regenerate everything

---
+++ run_analysis.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $NUMBER_OF_RUNS = 7;
+my $run_index = 0;
+
+for($run_index = 0; $run_index < $NUMBER_OF_RUNS; $run_index++) {
+  system("root -l -b -q processing/data_event_prdf_analysis/macros/Run_BeamWidthTime.C\'($run_index)\'");
+}
